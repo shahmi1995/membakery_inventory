6a9f171ceb27fff0ee947613d2f06431600156e7
mohamad shahmi <amieparkson@gmail.com>
2019-01-06 10:17:12 +0800

    Add project files.

---
+++ membakery_inventory/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(membakery_inventory.Startup))]
+namespace membakery_inventory
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
